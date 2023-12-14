25f2018d881a593aa72a371629280cd10b9ebf83
Ulli Hochholdinger <calvin@akk.org>
2023-12-14 22:37:44 +0100

    chore(aoc-13): day13 silver solved

--- day13/solution.go
+++ day13/solution.go
@@ -12,6 +12,7 @@ type mirror struct {
 	p *pattern
 	l int
 }
+
 func readLines() []string {
 	rawData, err := os.ReadFile("data.txt")
 	if err != nil {
@@ -20,53 +21,80 @@ func readLines() []string {
 	return strings.Split(string(rawData), "\n")
 }
 
-
-func (p *pattern) find_horizontal_mirror() *mirror {
+func (p *pattern) find_mirror() (bool,int) {
 	for i, l := range *p {
-		if i-1 > 0 {
+		fmt.Println("Zeile: ",l)
+		if i >= 1 {
 			if l == (*p)[i-1] {
-				fmt.Println(i,": ",l)
-				m := mirror{p,i}
-				return  &m
+				fmt.Println(i, ": ", l)
+				ret := true
+				for m,n := i, i-1; m <= len(*p)-1 && n >= 0 ; m,n = m+1,n-1 {
+					fmt.Println("testing(",len(*p),"): ",m,"|",n)
+					if (*p)[m] != (*p)[n] {
+						ret = false
+						break
+					}
+				}
+				if ret { return ret,i} 	
 			}
 		}
 	}
-	return &mirror{p,0}
+	return false,0
 }
 
-func (m *mirror) test_horizontal_mirror() (bool,int) {
-	if m.l == 0 {
-		return false,0
-	}
-	for i,j := m.l,m.l-1 ; i < len(*(m.p))-1 && j >= 0 ; i,j = i+1,j-1 {
-		fmt.Println((*(m.p))[i])
-		fmt.Println((*(m.p))[j])
-		if (*(m.p))[i] != (*(m.p))[j] {
-			fmt.Println("falsch")
-			return false,0
-		}
-	}
-	fmt.Println("getestet:",m.l)
-	return true,m.l
-}
+// func (p *pattern) find_horizontal_mirror() *mirror {
+// 	for i, l := range *p {
+// 		if i-1 > 0 {
+// 			if l == (*p)[i-1] {
+// 				fmt.Println(i, ": ", l)
+// 				m := mirror{p, i}
+// 				return &m
+// 			}
+// 		}
+// 	}
+// 	return &mirror{p, 0}
+// }
+
+// func (m *mirror) test_horizontal_mirror() (bool, int) {
+// 	if m.l == 0 {
+// 		return false, 0
+// 	}
+// 	for i, j := m.l, m.l-1; i < len(*(m.p))-1 && j >= 0; i, j = i+1, j-1 {
+// 		fmt.Println((*(m.p))[i])
+// 		fmt.Println((*(m.p))[j])
+// 		if (*(m.p))[i] != (*(m.p))[j] {
+// 			fmt.Println("falsch")
+// 			return false, 0
+// 		}
+// 	}
+// 	fmt.Println("getestet:", m.l)
+// 	return true, m.l
+// }
 
 func (p *pattern) rotate() *pattern {
+	for _,l := range *p {
+	fmt.Println(l)
+	}
 	np := make(pattern, 0)
-
-	for i := 0 ; i < len((*p)[0])-1 ; i++ {
+	fmt.Println("rotating")
+	for i := 0; i < len((*p)[0]); i++ {
 		var line strings.Builder
-		 for _,j := range (*p) {
-			line.WriteByte(j[i])
-		 }
-		 np = append(np, line.String())
-	} 
+		for j := len(*p)-1 ; j >=0 ; j--  {
+			line.WriteByte((*p)[j][i])
+		}
+		np = append(np, line.String())
+	}
+	for _,l := range np {
+		fmt.Println(l)
+		}
+	
 	return &np
 }
 
 func main() {
 	p := make([]pattern, 1)
 	num := 0
-	silver :=0
+	silver := 0
 	for _, line := range readLines() {
 		if line == "" {
 			num += 1
@@ -76,19 +104,21 @@ func main() {
 		p[num] = append(p[num], line)
 
 	}
-	fmt.Println("Anzahl: ",len(p))
+	fmt.Println("Anzahl: ", len(p))
 	for i, t := range p {
-		m,l := (&t).find_horizontal_mirror().test_horizontal_mirror()
+		fmt.Println("Pattern: ",i)
+		m, l := (&t).find_mirror()
 		if m {
-			silver += (l*100)
-			fmt.Println("hPattern: ",i,l*100)
-		}
-		m,l = (&t).rotate().find_horizontal_mirror().test_horizontal_mirror()
+			silver += (l * 100)
+			fmt.Println("hPattern: ", i, l*100)
+		} else {
+			m, l = (&t).rotate().find_mirror()
 			if m {
 				silver += l
-				fmt.Println("vPattern: ",i,l)
+				fmt.Println("vPattern: ", i, l)
 			}
-		
+
+		}
 	}
 
 	fmt.Println(silver)

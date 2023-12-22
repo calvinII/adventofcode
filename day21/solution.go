4cf5ddacf50bb6d570e27706182facb8b3b645a7
Ulli Hochholdinger <calvin@akk.org>
2023-12-22 10:59:44 +0100

    chore(aoc-21): experimenting with different values to get the diamond structure

--- day21/solution.go
+++ day21/solution.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func readLines() []string {
@@ -21,23 +22,25 @@ type coord struct {
 
 func (c *coord) neighs() []coord {
 	return []coord{
-		coord{c.x - 1, c.y},
-		coord{c.x + 1, c.y},
-		coord{c.x, c.y - 1},
-		coord{c.x, c.y + 1},
+		{c.x - 1, c.y},
+		{c.x + 1, c.y},
+		{c.x, c.y - 1},
+		{c.x, c.y + 1},
 	}
 }
 
-func fields(s *map[coord]string, garden *map[coord]string) map[coord]string {
+func fields(s *map[coord]string, garden *map[coord]string, width int, height int, padding coord) *map[coord]string {
 	result := make(map[coord]string)
 	for k, _ := range *s {
 		for _, co := range k.neighs() {
-			if (*garden)[co]=="." {
-				result[co] ="O"
+			mCo := coord{(co.x + padding.x) % width, (co.y + padding.y) % height}
+			// fmt.Println(co, ":", mCo, (*garden)[mCo])
+			if (*garden)[mCo] == "." {
+				result[co] = "O"
 			}
 		}
 	}
-	return result
+	return &result
 }
 
 func main() {
@@ -49,7 +52,10 @@ func main() {
 	foundStart := false
 	garden := make(map[coord]string)
 	startMap := make(map[coord]string)
+	width := len(readLines()[0])
+	height := len(readLines())
 	for i, l := range readLines() {
+
 		for _, j := range gpMatch.FindAllStringIndex(l, -1) {
 			garden[coord{i, j[0]}] = "."
 		}
@@ -59,20 +65,32 @@ func main() {
 		if !foundStart {
 			s := sMatch.FindStringIndex(l)
 			if s != nil {
-				start = coord{s[0], i}
+				start = coord{i, s[0]}
 				garden[start] = "."
 				startMap[start] = "O"
 				foundStart = true
 			}
 		}
 	}
-
-	for i :=1 ; i <=64 ; i++ {
-		startMap = fields(&startMap, &garden)
+	startTime := time.Now()
+	sMap := &startMap
+	steps := 524
+	padding := coord{steps * width, steps * height}
+	for i := 1; i <= steps; i++ {
+		sMap = fields(sMap, &garden, width, height, padding)
+		// fmt.Println(sMap)
 	}
+	elapsed := time.Since(startTime)
+
+	// data is 131 x 131 
+	// steps to walk: 26501365
+	// 26501365/131 = 202300
+	// 202300x202300 data fileds
+
 	fmt.Println(startMap)
 	fmt.Println(garden)
 	fmt.Println(start)
-	fmt.Println("silver:",len(startMap))
+	fmt.Println("silver:", len(*sMap))
+	fmt.Println("elapsed: ", elapsed)
 
 }
